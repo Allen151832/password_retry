51a2fef15cb5aa95be4a3eed2684eb2891a7e68c
unknown <a32012841@gmail.com>
2023-04-24 22:11:46 +0800

    Final version

--- password.py
+++ password.py
@@ -6,12 +6,16 @@ i = 3 # 剩餘機會
 password = "a123456"
 while i > 0 :
 	pwd = input("請輸入密碼: ")
-	if pwd == password :
+	if pwd == password:
 		print("登入成功")
-		break
-	else :
+		breaks
+	else:
 		i -= 1 
-		print ("密碼錯誤!","還有",i,"次機會")
+		print ("密碼錯誤!")
+		if i > 0: 
+			print ("還有" ,i , "次機會")
+		else:
+			print ("您的帳號已被鎖定!")
 		
 		
 		
